cccfc3c0478fd8117555865ffe5303280bdb6867
sarud14 <ruzddchan@yahoo.com>
2025-07-13 22:37:44 +0700

    add sigint shutdown

--- E-learning_Server-API/src/server.js
+++ E-learning_Server-API/src/server.js
@@ -7,3 +7,9 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, ()=> {
   console.log(`server is running @ http://localhost:${PORT}/`)
 })
+
+process.on("SIGINT", ()=>shutdown("SIGINT"))
+process.on("SIGTERM", ()=>shutdown("SIGTERM"))
+
+process.on("uncaughtException", ()=>shutdown("uncaughtException"))
+process.on("unhandledRejection", ()=>shutdown("unhandledRejection"))
